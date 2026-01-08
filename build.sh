edd52704ad1db9a83730437df769051e5cb75a3d
Sudharsan <jssudharsan7@gmail.com>
2026-01-08 16:10:07 +0530

    fix: Force migrations and create django_site table

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "==> Installing packages..."
 pip install -r requirements.txt
+
+echo "==> Collecting static files..."
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+echo "==> Making migrations..."
+python manage.py makemigrations
+
+echo "==> Running migrations..."
+python manage.py migrate --run-syncdb
+
+echo "==> Creating default site..."
+python manage.py shell -c "
+from django.contrib.sites.models import Site
+site = Site.objects.get_or_create(id=1, defaults={'domain': 'chat-web-application-11a4.onrender.com', 'name': 'Real-Time Chat'})
+print('Site created/updated')
+"
+
+echo "==> Build complete!"
